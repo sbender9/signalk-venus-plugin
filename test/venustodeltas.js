a2e4f8429a44bfd3f5cdd487726ca6f3534c8b98
Scott Bender <scott@scottbender.net>
2017-11-27 20:16:04 -0500

    implement some real mappings to signalk

--- test/venustodeltas.js
+++ test/venustodeltas.js
@@ -43,7 +43,8 @@ describe('venustodeltas', function () {
           ]
         ],
         text: '4459.1',
-        value: 4459.099999998691
+        value: 4459.099999998691,
+        senderName: '1'
       })
 
       expect(deltas.length).to.equal(1)
@@ -51,6 +52,234 @@ describe('venustodeltas', function () {
         'updates[0].values[0]': {
           path: 'electrical.batteries.1.voltage',
           value: 4459.099999998691
+        }
+      })
+    })
+  })
+
+  describe('Battery Current', function () {
+    it('should return curent in normal case', function () {
+      const deltas = venusToDeltas({
+        "serial": 22,
+        "path": "/Dc/0/Current",
+        "interface": "com.victronenergy.BusItem",
+        "member": "PropertiesChanged",
+        "signature": "a{sv}",
+        "sender": ":1.50",
+        "type": 4,
+        "flags": 1,
+        "body": [
+          [
+            [
+              "Text",
+              [
+                [
+                  {
+                    "type": "s",
+                    "child": []
+                  }
+                ],
+                [
+                  "-81.0A"
+                ]
+              ]
+            ],
+            [
+              "Value",
+              [
+                [
+                  {
+                    "type": "d",
+                    "child": []
+                  }
+                ],
+                [
+                  -81
+                ]
+              ]
+            ]
+          ]
+        ],
+        "text": "-81.0A",
+        "value": -81,
+        "senderName": 1
+      })
+      expect(deltas.length).to.equal(1)
+      expect(deltas[0]).to.nested.deep.include({
+        'updates[0].values[0]': {
+          path: 'electrical.batteries.1.current',
+          value: -81
+        }
+      })
+    })
+  })
+
+  describe('Battery State Of Charge', function () {
+    it('should return state of charge in normal case', function () {
+      const deltas = venusToDeltas({
+        "serial": 20,
+        "path": "/Soc",
+        "interface": "com.victronenergy.BusItem",
+        "member": "PropertiesChanged",
+        "signature": "a{sv}",
+        "sender": ":1.54",
+        "type": 4,
+        "flags": 1,
+        "body": [
+          [
+            [
+              "Text",
+              [
+                [
+                  {
+                    "type": "s",
+                    "child": []
+                  }
+                ],
+                [
+                  "58.1%"
+                ]
+              ]
+            ],
+            [
+              "Value",
+              [
+                [
+                  {
+                    "type": "d",
+                    "child": []
+                  }
+                ],
+                [
+                  58.099998474121094
+                ]
+              ]
+            ]
+          ]
+        ],
+        "text": "58.1%",
+        "value": 58.099998474121094,
+        "senderName": 1
+      })
+      expect(deltas.length).to.equal(1)
+      expect(deltas[0]).to.nested.deep.include({
+        'updates[0].values[0]': {
+          path: 'electrical.batteries.1.capacity.stateOfCharge',
+          value: .58099998474121094
+        }
+      })
+    })
+  })
+
+  describe('Time Remaining', function () {
+    it('should return time remaining in normal case', function () {
+      const deltas = venusToDeltas({
+        "serial": 21,
+        "path": "/TimeToGo",
+        "interface": "com.victronenergy.BusItem",
+        "member": "PropertiesChanged",
+        "signature": "a{sv}",
+        "sender": ":1.53",
+        "type": 4,
+        "flags": 1,
+        "body": [
+          [
+            [
+              "Text",
+              [
+                [
+                  {
+                    "type": "s",
+                    "child": []
+                  }
+                ],
+                [
+                  "19740s"
+                ]
+              ]
+            ],
+            [
+              "Value",
+              [
+                [
+                  {
+                    "type": "d",
+                    "child": []
+                  }
+                ],
+                [
+                  19739.998046875
+                ]
+              ]
+            ]
+          ]
+        ],
+        "text": "19740s",
+        "value": 19739.998046875,
+        "senderName": 1
+      })
+      expect(deltas.length).to.equal(1)
+      expect(deltas[0]).to.nested.deep.include({
+        'updates[0].values[0]': {
+          path: 'electrical.batteries.1.capacity.timeRemaining',
+          value: 19739.998046875
+        }
+      })
+    })
+  })
+
+  describe('Panel Current', function () {
+    it('should return panel curent in normal case', function () {
+      const deltas = venusToDeltas({
+        "serial": 28,
+        "path": "/Pv/I",
+        "interface": "com.victronenergy.BusItem",
+        "member": "PropertiesChanged",
+        "signature": "a{sv}",
+        "sender": ":1.49",
+        "type": 4,
+        "flags": 1,
+        "body": [
+          [
+            [
+              "Text",
+              [
+                [
+                  {
+                    "type": "s",
+                    "child": []
+                  }
+                ],
+                [
+                  "16.1A"
+                ]
+              ]
+            ],
+            [
+              "Value",
+              [
+                [
+                  {
+                    "type": "d",
+                    "child": []
+                  }
+                ],
+                [
+                  16.143278121948242
+                ]
+              ]
+            ]
+          ]
+        ],
+        "text": "16.1A",
+        "value": 16.143278121948242,
+        "senderName": "1"
+      })
+      expect(deltas.length).to.equal(1)
+      expect(deltas[0]).to.nested.deep.include({
+        'updates[0].values[0]': {
+          path: 'electrical.chargers.1.panelCurrent',
+          value: 16.143278121948242
         }
       })
     })
--- venusToDeltas.js
+++ venusToDeltas.js
@@ -1,17 +1,49 @@
+
+const mappings = {
+  '/Dc/0/Voltage': {
+    path: 'electrical.batteries.${instance}.voltage'
+  },
+  '/Dc/1/Voltage': {
+    path: 'electrical.batteries.${instance}.voltage'
+  },
+  "/Dc/0/Current": {
+    path: 'electrical.batteries.${instance}.current'
+  },
+  '/Soc': {
+    path: 'electrical.batteries.${instance}.capacity.stateOfCharge',
+    conversion: percentToRatio
+  },
+  '/TimeToGo': {
+    path: 'electrical.batteries.${instance}.capacity.timeRemaining'
+  },
+  '/Pv/I': {
+    path: 'electrical.chargers.${instance}.panelCurrent'
+  }
+}
+
 module.exports = function (venusMessage) {
   if (venusMessage.interface != 'com.victronenergy.BusItem' ||
       venusMessage.member != 'PropertiesChanged')
     return []
 
+  var mapping = mappings[venusMessage.path]
+  if ( !mapping || !venusMessage.senderName )
+    return []
+
+  var instance = venusMessage.senderName
+  var theValue = venusMessage.value
+
+  if ( mapping.conversion )
+    theValue = mapping.conversion(theValue)
+  
   return [
     {
       updates: [
         {
-          $source: 'plugins.venus-to-signalk',
           values: [
             {
-              path: 'electrical.batteries.1.voltage',
-              value: Number(venusMessage.value)
+              path: mapping.path.replace(/\$\{instance\}/g, instance),
+              value: theValue
             }
           ]
         }
@@ -19,3 +51,8 @@ module.exports = function (venusMessage) {
     }
   ]
 }
+
+function percentToRatio(percent) {
+  return percent / 100.0
+}
+
