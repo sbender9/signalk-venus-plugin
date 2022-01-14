a96377ce60e9508fcd16e87722c298131d29e640
Scott Bender <scott@scottbender.net>
2022-01-14 10:13:17 -0500

    chore: fix formatting

--- venusToDeltas.js
+++ venusToDeltas.js
@@ -655,13 +655,13 @@ module.exports = function (app, options, handleMessage) {
     } else if ( msg.senderName.startsWith('com.victronenergy.hub4')) {
       type = 'hub4'
     } else if ( msg.senderName.startsWith('com.victronenergy.dcsource')) {
-        type = 'dcsource'
+      type = 'dcsource'
     } else if ( msg.senderName.startsWith('com.victronenergy.dcload')) {
-        type = 'dcload'
+      type = 'dcload'
     } else if ( msg.senderName.startsWith('com.victronenergy.alternator')) {
-        type = 'alternator'
+      type = 'alternator'
     } else if ( msg.senderName.startsWith('com.victronenergy.dcdc')) {
-        type = 'dcdc'
+      type = 'dcdc'
     } else {
       app.debug('no path for %s', msg.senderName)
       return null
