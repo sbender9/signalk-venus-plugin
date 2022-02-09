3510df861ff1a6d627b682c80f09ea1ac70dc203
Scott Bender <scott@scottbender.net>
2022-02-09 07:29:38 -0500

    feature: support /State values without plugin mappings (#102)

--- venusToDeltas.js
+++ venusToDeltas.js
@@ -96,7 +96,10 @@ module.exports = function (app, options, handleMessage) {
       },
       {
         path: m => {
-          return makePath(m, `${m.instanceName}.${getStatePropName(m)}Number`)
+          let propName = getStatePropName(m)
+          if ( propName ) {
+            return makePath(m, `${m.instanceName}.${propName}Number`)
+          }
         }
       },
 
@@ -826,7 +829,7 @@ function isVEBus (msg) {
 
 function convertState (msg, forInverter) {
   var map = stateMaps[senderNamePrefix(msg.senderName)]
-  return (map && map[Number(msg.value)]) || 'unknown'
+  return (map && map[Number(msg.value)]) || msg.value
 }
 
 function convertStateForVEBusInverter (msg) {
@@ -884,7 +887,7 @@ const statePropName = {
 }
 
 function getStatePropName (msg) {
-  return statePropName[senderNamePrefix(msg.senderName)]
+  return statePropName[senderNamePrefix(msg.senderName)] || 'state'
 }
 
 function convertMode (msg) {
