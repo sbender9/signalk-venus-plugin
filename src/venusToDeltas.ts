027ca63d8eed19993abf594d8e4be5bfa1d219b2
Scott Bender <scott@scottbender.net>
2026-05-14 19:16:44 -0400

    fix: add support for custom names in DC-DC services

--- src/venusToDeltas.ts
+++ src/venusToDeltas.ts
@@ -529,6 +529,7 @@ export function convertRuuivStatus(msg: Message) {
 const servicesWithCustomNames: string[] = [
   'com.victronenergy.battery',
   'com.victronenergy.dcload',
+  'com.victronenergy.dcdc',
   'com.victronenergy.solarcharger',
   'com.victronenergy.inverter',
   'com.victronenergy.vebus',
